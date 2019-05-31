b386119fa2a86741943791313b534cc92b9859ca
Amos Wenger <amoswenger@gmail.com>
2019-05-31 11:33:38 +0200

    Initial import

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![feature(async_await)]
+#![warn(clippy::all)]
+
+mod message;
+pub use message::*;
+
+mod system;
+pub use system::*;
+
+mod error;
+pub use error::*;
+
+pub use chrono::DateTime;
